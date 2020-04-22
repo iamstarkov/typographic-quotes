b2f3341d872e5dcf582fcfc99ff5b78627ea7667
Jake Low <hello@jakelow.com>
2020-04-21 20:13:30 -0700

    refactor to make separator const

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ export default function typographicQuotes(input = '', { locale } = {}) {
     return input;
   }
   const localeQuotes = db[locale];
-  let separator = '';
+  const separator = locale === 'fr' ? ' ' : '';
 
   const pattern = /(?<=^|\s|[\[(\-–—])(?:"(.*?)"|'(.*?)')(?=$|\s|[.,:;?!…\])\-–—])/gim;
   const handleQuotes = (quotes, cb) =>
     (match, part1='', part2='') => {
       let text = (part1 + part2);
       if (cb) { text = text.replace(pattern, cb); }
-      if (locale === 'fr') { separator = ' '; }
       return `${quotes[0]}${separator}${text}${separator}${quotes[1]}`;
     }
 
